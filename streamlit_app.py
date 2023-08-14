2afdad1c110589c3b0998d54e2cf0c4a0f86c032
GSejas <jsequeira03@gmail.com>
2023-08-13 19:07:40 -0600

    Update streamlit_app.py
    simple post

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,22 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import requests
 
-"""
-# Welcome to Streamlit!
+# Check if the request method is POST
+if st.request.method == "POST":
+    data = st.request.json
+    # Extract the required data or parameters from AppSheet's webhook payload
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+    # Forward this data to Google Apps Script
+    google_apps_script_url = "https://script.googleapis.com/v1/scripts/[YOUR_SCRIPT_ID]:run"
+    headers = {
+        "Authorization": "Bearer YOUR_OAUTH_TOKEN",
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "function": "YourFunctionName",
+        "parameters": [data['param1'], data['param2']] # modify based on your needs
+    }
+    response = requests.post(google_apps_script_url, headers=headers, json=payload)
+    st.write(response.json())
+else:
+    st.write("This is a webhook endpoint. Please POST data to it.")
